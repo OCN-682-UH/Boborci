aba792bb42551166b1beb273535b6953c7366c30
madsbobo <mboborci@hawaii.edu>
2025-09-08 23:08:50 -1000

    Week_02

--- Week_02/Scripts/Week02_Script.R
+++ Week_02/Scripts/Week02_Script.R
@@ -5,7 +5,7 @@
 ### Load Libraries ###
 library(tidyverse)
 library(here)
-### Reas in da data###
+### Read in da data###
 WeightData<-read_csv(here("Week_02","Data", "weightdata.csv"))
 ### Analyze ###
 head(WeightData) ###shows top 6 lines of data set 
